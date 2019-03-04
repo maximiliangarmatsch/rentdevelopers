219a2637d18cb6e656064f624219b4d8387c88f1
miljan <miljantodorovic.bbs@gmail.com>
2019-03-04 13:33:04 +0100

    Changing login form

--- src/components/developer/Login.js
+++ src/components/developer/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 
 class Login extends Component {
   state = {
@@ -45,15 +46,42 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Header</h1>
-        <form onSubmit={this.onSubmit}>
-          <input id="title" type="text" placeholder="title" />
-          <input id="content" type="text" placeholder="content" />
-          <input type="submit" />
-        </form>
-        <br />
-      </div>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="6" className="login">
+              <MDBCard>
+                <MDBCardBody>
+                  <form>
+                    <p className="h4 text-center py-4">Login</p>
+                    <div className="grey-text">
+                      <MDBInput
+                          label="Your name"
+                          icon="user"
+                          group
+                          type="text"
+                          validate
+                          error="wrong"
+                          success="right"
+                      />
+                      <MDBInput
+                          label="Your password"
+                          icon="lock"
+                          group
+                          type="password"
+                          validate
+                      />
+                    </div>
+                    <div className="text-center py-4 mt-3">
+                      <MDBBtn color="cyan" type="submit">
+                        Login
+                      </MDBBtn>
+                    </div>
+                  </form>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
     )
   }
 }
