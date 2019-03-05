62804ec987e6b42ad6bdf7eb233398e6df3588da
MegaKreja <kreja95ue@gmail.com>
2019-03-05 16:06:28 +0100

    added inputs

--- src/components/client/HireForm/HireForm.js
+++ src/components/client/HireForm/HireForm.js
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardUp, MDBCardImage, MDBCardBody, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBInput, MDBCardBody, MDBBtn } from "mdbreact";
 import RangeCalendar from 'rc-calendar/lib/RangeCalendar';
 import 'rc-calendar/assets/index.css';
 import "./HireForm.css";
 
 class HireForm extends Component {
+	state = {
+		hours: 0,
+		cost: 0
+	}
+
 	render() {
 		return (
 			<MDBContainer fluid className="hireForm">
-
 						<MDBCard className="hireFormCard">
 							<h1>Hire Form</h1>
 							<MDBCardBody>
@@ -16,6 +20,17 @@ class HireForm extends Component {
 									<MDBCol>
 										<h2>Choose date</h2>
 										<RangeCalendar />
+										<h4>Daily work hours</h4>
+										<MDBInput type="number" outline/>
+										<h4>How many working days throughout this week </h4>
+										<MDBInput type="number" outline/>
+										<h4>Total hours: {this.state.hours}/h</h4>
+										<hr/>
+										<h1>Total Cost: ${this.state.cost}</h1>
+										<div className="hireFormSubmit">
+											<MDBBtn color="primary">Make an order</MDBBtn>
+										</div>
+
 									</MDBCol>
 								</MDBRow>
 							</MDBCardBody>
