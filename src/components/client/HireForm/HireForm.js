c459a0d62d8cdef32e71a92ce9525a28cf6849f8
MegaKreja <kreja95ue@gmail.com>
2019-03-07 12:41:22 +0100

    fixed bug in label

--- src/components/client/HireForm/HireForm.js
+++ src/components/client/HireForm/HireForm.js
@@ -8,17 +8,16 @@ const formatStr = 'YYYY-MM-DD';
 function format(v) {
 	return v ? v.format(formatStr) : '';
 }
-
-function onStandaloneSelect(value) {
-	console.log('onSelect');
-	console.log(format(value[0]), format(value[1]));
-
-	const date2 = new Date(format(value[0]));
-	const date1 = new Date(format(value[1]));
-	const timeDiff = Math.abs(date2.getTime() - date1.getTime());
-	window.dayDifference = Math.ceil(timeDiff / (1000 * 3600 * 24));
-	// console.log("Dana oznaceno:" + dayDifference);
-}
+// function onStandaloneSelect(value) {
+// 	console.log('onSelect');
+// 	console.log(format(value[0]), format(value[1]));
+//
+// 	const date2 = new Date(format(value[0]));
+// 	const date1 = new Date(format(value[1]));
+// 	const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+// 	window.dayDifference = Math.ceil(timeDiff / (1000 * 3600 * 24));
+// 	// console.log("Dana oznaceno:" + dayDifference);
+// }
 
 class HireForm extends Component {
 	state = {
@@ -41,20 +40,19 @@ class HireForm extends Component {
 		value = Math.max(Number(min), Math.min(Number(max), Number(value)));
 		this.setState({dailyInput: value });
 	}
+
 	onChangeWeekly = (e) => {
 		let { value, min } = e.target;
-		const dayDifference = window.dayDifference;
-		value = Math.max(Number(min), Math.min(Number(dayDifference), Number(value)));
+		value = Math.max(Number(min), Math.min(Number(this.state.dayDifference), Number(value)));
 		this.setState({weeklyInput: value})
 	}
 
 	render() {
-		console.log(window.dayDifference);
-		const {dailyInput, weeklyInput} = this.state;
+		const {dailyInput, weeklyInput, dayDifference} = this.state;
 		const cost = this.props.location.state.cost ? this.props.location.state.cost : 0;
 		const totalHours = dailyInput * weeklyInput;
 		const totalCost = totalHours * cost;
-		const labelString = window.dayDifference ? `Max ${window.dayDifference} days` : "Fill the calendar"
+		const labelString = dayDifference ? `Max ${dayDifference} days` : "Fill the calendar"
 		return (
 			<MDBContainer fluid className="hireForm">
 						<MDBCard className="hireFormCard">
@@ -63,7 +61,7 @@ class HireForm extends Component {
 								<MDBRow>
 									<MDBCol>
 										<h2>Choose date</h2>
-										<RangeCalendar onChange={this.onChange} onSelect={onStandaloneSelect}/>
+										<RangeCalendar onChange={this.onChange} />
 										<MDBInput value={dailyInput ? String(dailyInput) : ""} min="1" max="24" onChange={this.onChangeDaily} label="Daily work hours" type="number" outline/>
 										<MDBInput value={weeklyInput ? String(weeklyInput) : ""} onChange={this.onChangeWeekly} min="1" label={`Working days through week(${labelString})`} type="number" outline/>
 										<h4>Total hours: {totalHours}/h</h4>
