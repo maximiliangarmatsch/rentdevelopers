a84ccc8fec4423a3f38fa9b5226a3ff2d15788b7
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-03-14 11:54:16 +0100

    Added navbar and footer to HireForm

--- src/components/client/HireForm/HireForm.js
+++ src/components/client/HireForm/HireForm.js
@@ -4,6 +4,8 @@ import {Link} from "react-router-dom";
 import RangeCalendar from 'rc-calendar/lib/RangeCalendar';
 import 'rc-calendar/assets/index.css';
 import "../../../styles/HireForm.css";
+import Header from "../../Header/Header";
+import Footer from "../../footer/Footer";
 
 const formatStr = 'YYYY-MM-DD';
 function format(v) {
@@ -58,7 +60,18 @@ class HireForm extends Component {
 		const totalCost = totalHours ? totalHours * cost : 0;
 		const labelString = dayDifference ? `Max ${dayDifference > 7 ? "7" : dayDifference} days` : "Fill the calendar";
 		return (
-			<MDBContainer fluid className="hireForm">
+			<div>
+				<Header
+					text1='User'
+					route1={`/developer/member/${localStorage.getItem('username')}`}
+					text4="Login"
+					text2='Logout'
+					route4="/developer/login"
+					text5="Register"
+					text3='Details'
+					route5="/developer/register"
+				/>
+				<MDBContainer fluid className="hireForm">
 						<MDBCard className="hireFormCard">
 							<h1>Hire Form</h1>
 							<MDBCardBody>
@@ -81,7 +94,9 @@ class HireForm extends Component {
 								</MDBRow>
 							</MDBCardBody>
 						</MDBCard>
-			</MDBContainer>
+				</MDBContainer>
+				<Footer/>
+			</div>
 		)
 	}
 }
