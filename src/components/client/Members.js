fcd23a74678b903a27a24ef07d5afc290411bf78
MegaKreja <kreja95ue@gmail.com>
2019-03-15 15:53:41 +0100

    members changed a little

--- src/components/client/Members.js
+++ src/components/client/Members.js
@@ -93,7 +93,6 @@ class Members extends Component {
 		const members = employees.map((member, i) => {
 			colorsIndex = colorsIndex >= colors.length - 1 ? -1 : colorsIndex;
 			colorsIndex += 1;
-			console.log(colorsIndex);
 			return (
 					<MDBCard className={pickedTeam.length < 1 ? "withoutSidebar" : "withSidebar"} key={i}>
 						<div className="memberUp" style={{background: `linear-gradient(${colors[colorsIndex]} 50%, transparent 50%) no-repeat`}}>
