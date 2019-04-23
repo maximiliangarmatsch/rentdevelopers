1f143bfd0e6342ca3a3ca6c6637c71d08a83bfd8
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-23 11:10:02 +0200

    Displaying checkboxes inside basic tab

--- src/components/Client/SkillTabPanel.js
+++ src/components/Client/SkillTabPanel.js
@@ -3,17 +3,45 @@ import {MDBRow,MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBConta
 
 class SkillTabPanel extends Component{
 	render () {
+
 		const data = this.props.data;
-		const lang = data.language.language_name;
+
+		const langName = data.language.language_name;
+		const basic = data.language.php.basic;
+		const array = Object.keys(basic).map(function(key) {
+			return [String(key), basic[key]];
+		});
+
+		const basicResults = array.map((row) => {
+			let name = "";
+			let checked = false;
+			 row.map((cell, index) => {
+				if (index == 0){
+					 name = cell;
+				}else {
+					if (cell){
+						checked = true;
+					}
+				}
+			});
+
+			return (
+				<div>
+				<input type="checkbox" name="skill" value={name} checked={checked}/> {name.replace("_"," ")}
+				<br/>
+				</div>
+			);
+		});
+
 		return (
 			<div>
-				{lang}
+				{langName}
 				<MDBRow>
 					<MDBCol>
 				<MDBContainer>
 					<MDBCard className="card-body" style={{  marginTop: "1rem" }}>
 						<MDBCardTitle>Basic</MDBCardTitle>
-						<MDBCardBody>Checkboxes will go here.</MDBCardBody>
+						<MDBCardBody>{basicResults}</MDBCardBody>
 					</MDBCard>
 				</MDBContainer>
 					</MDBCol>
